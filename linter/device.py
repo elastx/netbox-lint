83b02f1bac352290b0d7e072d4ea3fa24461c096
bl0m1 <hugo@blommail.com>
2021-06-08 16:51:40 +0200

    split rules into multiple classes

--- linter/device.py
+++ linter/device.py
@@ -22,32 +22,75 @@ class DeviceNamingRule:
             return
         if not re.match(r'[a-z0-9-]+', device.name):
             yield 'Name contains invalid characters'
-        if device.site["id"] is None:
-            yield 'Device must be located in a site'
-        if device.asset_tag is None:
-            yield 'Device must have an asset tag'
-        if device.device_role["name"] == "Compute":
-            if device.primary_ip is None:
-                yield 'Devices with status "active" must have a primary IP'
-            if device.status["value"] == "staged":
-                if device.platform is not None:
-                    yield 'Devices with status "staged" should not be assigned a platform'
-            if device.status["value"] == "active":
-                if device.platform is None:
-                    yield 'Devices with status "active" should not be assigned a platform'  
-        if device.device_role["name"] == "HSM":
-            if device.primary_ip is None:
-                yield 'Devices with status "active" must have a primary IP'
-        if device.device_role["name"] == "Storage":
-            if device.primary_ip is None:
-                yield 'Devices with status "active" must have a primary IP'
-        if device.device_role["name"] == "Network":
-            if device.primary_ip is None:
-                yield 'Devices with status "active" must have a primary IP' 
         if len(device.name) < 3:
             yield 'Name is too short'
 
+class DeviceLocationRule:
+    ID = 'DEVICE-LOCATION'
+
+    def __init__(self, settings: util.RuleSetting):
+        pass
+
+    def check(self, device: pynetbox.models.dcim.Devices) -> Iterator[str]:
+        if device.site['id'] is None:
+            yield 'Device must be located in a site'
+
+class DeviceAssetTagRule:
+    ID = 'DEVICE-ASSETTAG'
+
+    def __init__(self, settings: util.RuleSetting):
+        pass
+
+    def check(self, device: pynetbox.models.dcim.Devices) -> Iterator[str]:
+        if device.asset_tag is None:
+            yield 'Device must have an asset tag'
+
+
+class DevicePrimaryIPRule:
+    ID = 'DEVICE-PRIMARYIP'
+
+    def __init__(self, settings: util.RuleSetting):
+        pass
+
+    def check(self, device: pynetbox.models.dcim.Devices) -> Iterator[str]:
+        if device.device_role['name'] == "Compute":
+            if device.primary_ip is None:
+                yield 'Devices with status "active" must have a primary IP'
+            if device.status['value'] == "staged":
+                if device.platform is not None:
+                    yield 'Devices with status "staged" should not be assigned a platform'
+            if device.status['value'] == "active":
+                if device.platform is None:
+                    yield 'Devices with status "active" should not be assigned a platform'  
+        if device.device_role['name'] == "HSM":
+            if device.primary_ip is None:
+                yield 'Devices with status "active" must have a primary IP'
+        if device.device_role['name'] == "Storage":
+            if device.primary_ip is None:
+                yield 'Devices with status "active" must have a primary IP'
+        if device.device_role['name'] == "Network":
+            if device.primary_ip is None:
+                yield 'Devices with status "active" must have a primary IP' 
+
+class DeviceStatusRule:
+    ID = 'DEVICE-STATUS'
+
+    def __init__(self, settings: util.RuleSetting):
+        pass
+
+    def check(self, device: pynetbox.models.dcim.Devices) -> Iterator[str]:
+        if device.device_role['name'] == "Compute":
+            if device.status['value'] == "staged":
+                if device.platform is not None:
+                    yield 'Devices with status "staged" should not be assigned a platform'
+            if device.status['value'] == "active":
+                if device.platform is None:
+                    yield 'Devices with status "active" should not be assigned a platform'  
 
 AllRules = [
-        DeviceNamingRule
+    DeviceNamingRule,
+    DeviceLocationRule,
+    DeviceAssetTagRule,
+    DevicePrimaryIPRule,
+    DeviceStatusRule
 ]
