c15c11070a29f443cba12566616dd47763fd8461
bl0m1 <hugo@blommail.com>
2021-06-08 17:41:24 +0200

    add unit tests for additional device checks

--- linter/device_test.py
+++ linter/device_test.py
@@ -7,8 +7,8 @@ from . import device as rules
 
 
 class FakeDevice(namedtuple(
-    'FakeDevice', ['name', 'status', 'device_role', 'platform'],
-    defaults=['Fake', {'value': 'toaster'}, {'name': 'Invalid'}, 'Fake'])):
+    'FakeDevice', ['name', 'status', 'device_role', 'platform', 'asset_tag', 'site', 'primary_ip'],
+    defaults=['Fake', {'value': 'toaster'}, {'name': 'Invalid'}, 'Fake', None, None, None])):
     """A fake device replicating the Netbox Device model."""
 
 
@@ -30,6 +30,57 @@ class TestDeviceNamingRule(unittest.TestCase):
         self.assertCountEqual(got, [
             'Name is too short',
         ])
+
+class TestDeviceLocationRule(unittest.TestCase):
+    """Tests the behavior of the DeviceLocationRule check."""
+
+    def test_with_site_name(self):
+        rule = rules.DeviceLocationRule({})
+        device = FakeDevice(site={'id': '1'})
+        got = list(rule.check(device))
+        self.assertCountEqual(got, [])
+
+class TestDeviceAssetTagRule(unittest.TestCase):
+    """Tests the behavior of the DeviceAssetTagRule check."""
+
+    def test_without_asset_tag(self):
+        rule = rules.DeviceAssetTagRule({})
+        device = FakeDevice()
+        got = list(rule.check(device))
+        self.assertCountEqual(got, [
+            'Device must have an asset tag',
+        ])
+
+    def test_with_valid_asset_tag(self):
+        rule = rules.DeviceAssetTagRule({})
+        device = FakeDevice(asset_tag='000001')
+        got = list(rule.check(device))
+        self.assertCountEqual(got, [])
+
+
+class TestDevicePrimaryIPRule(unittest.TestCase):
+    """Tests the behavior of the DevicePrimaryIPRule check."""
+
+    def test_invalid_role(self):
+        rule = rules.DevicePrimaryIPRule({})
+        device = FakeDevice()
+        got = list(rule.check(device))
+        self.assertCountEqual(got, [])
+
+    def test_invalid_primaryip(self):
+        rule = rules.DevicePrimaryIPRule({})
+        device = FakeDevice(device_role={'name': 'Compute'})
+        got = list(rule.check(device))
+        self.assertCountEqual(got, [
+            'Devices with role "Compute" must have a primary IP',
+        ])
+
+    def test_valid(self):
+        rule = rules.DevicePrimaryIPRule({})
+        device = FakeDevice(device_role={'name': 'Compute'}, primary_ip="10.10.10.0/24")
+        got = list(rule.check(device))
+        self.assertCountEqual(got, [])
+
 
 class TestDeviceLifecycleRule(unittest.TestCase):
     """Tests the behavior of the DeviceLifecycleRule check."""
